897b3c9987a864843f253fb896e48da8351f43a0
vidura samitha <108300369+vidura99@users.noreply.github.com>
2023-09-20 23:58:04 +0530

    sample bill calculater

---
+++ src/main/java/com/mycompany/billingsys/Billingsys.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.billingsys;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Billingsys {
+
+    public static void main(String[] args) {
+        
+    }
+}
